279192a7c4896556aaa3e2baf8e564c3519d70c7
priyanshsingh <singh.priyansh1391@gmail.com>
2025-08-01 00:17:31 +0530

    change

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
 
-const Card = ({ data }) => {
+const Card = ({ data = [] }) => {
   console.log(data);
-  
-  return (
 
-    <div className='flex justify-center gap-10 flex-wrap mt-3' >
-      { data.map((curItem, index) => (
+  return (
+    <div className='flex justify-center gap-10 flex-wrap mt-3'>
+      {Array.isArray(data) && data.map((curItem, index) => (
         <div key={index}>
-          <img className='w-full h-[180px]'  src={curItem.urlToImage} alt="news" />
+          <img className='w-full h-[180px]' src={curItem.urlToImage} alt="news" />
           <div className="card w-80 bg-white mt-5 border-black">
-            <a className='font-medium size-[18px] mt-[-7px] hover:[border-bottom] cursor-pointer' onClick={()=>window.open(curItem.url)} >{curItem.title }</a>
+            <a
+              className='font-medium size-[18px] mt-[-7px] hover:[border-bottom] cursor-pointer'
+              onClick={() => window.open(curItem.url)}
+            >
+              {curItem.title}
+            </a>
             <p className='p-2'>{curItem.description}</p>
-            <button onClick={()=>window.open(curItem.url)} className="cursor-pointer rounded-md bg-blue-700 text-white px-2 py-1 hover:bg-blue-800 transition">Read More</button>
+            <button
+              onClick={() => window.open(curItem.url)}
+              className="cursor-pointer rounded-md bg-blue-700 text-white px-2 py-1 hover:bg-blue-800 transition"
+            >
+              Read More
+            </button>
           </div>
         </div>
       ))}
     </div>
-    
   );
 };
 
